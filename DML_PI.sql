7079d02f9defc2205a02e54190fe8cc62a1dbf61
johnwickuau <joaoguitins11@gmail.com>
2026-03-19 17:19:10 -0300

    Update DML_PI.sql

--- DML_PI.sql
+++ DML_PI.sql
@@ -1,54 +1,61 @@
-desc equipes;
-INSERT INTO equipes (nome, codigo) 
-VALUES ("audi","a"),
-("ferrari","b"),
-("red bull","c"),
-("mercedes","d"),
-("mclares","e");
-desc participantes;
+	desc equipes;
+	INSERT INTO equipes (nome, codigo) 
+	VALUES ("audi","a"),
+	("ferrari","b"),
+	("red bull","c"),
+	("mercedes","d"),
+	("mclares","e");
+	desc participantes;
 
-desc pistas;
+	desc pistas;
 
-INSERT INTO pistas (nome, id_equipe) 
-VALUES("suzuka",1),
-("interlagos",2),
-("lindblad",3),
-("zandvorten",4),
-("america",5);
+	INSERT INTO pistas (nome, id_equipe) 
+	VALUES("suzuka",1),
+	("interlagos",2),
+	("lindblad",3),
+	("zandvorten",4),
+	("america",5);
 
 
-INSERT INTO participantes (nome) 
-VALUES("joao"),
-("marcos"),
-("max"),
-("eduarda"),
-("isabella"),
-("gabriel"),
-("joao.v"),
-("enzo"),
-("lucas");
+	INSERT INTO participantes (nome) 
+	VALUES("joao"),
+	("marcos"),
+	("max"),
+	("eduarda"),
+	("isabella"),
+	("gabriel"),
+	("joao.v"),
+	("enzo"),
+	("lucas");
 
-desc equipes_participantes;
+	desc equipes_participantes;
 
-INSERT INTO equipes_participantes (id_participante, id_equipe) 
-VALUES("1",1),
-(4,1),
-(5,1),
-(6,1),
-(2,2),
-(3,3),
-(7,4),
-(8,5);
+	INSERT INTO equipes_participantes (id_participante, id_equipe) 
+	VALUES("1",1),
+	(4,1),
+	(5,1),
+	(6,1),
+	(2,2),
+	(3,3),
+	(7,4),
+	(8,5);
 
-desc corridas;
+	desc corridas;
 
-INSERT INTO corridas (nome)
-VALUES ("corrida 1"),
- ("corrida 2"),
- ("corrida 3"),
- ("corrida 4"),
- ("corrida 5");
+	INSERT INTO corridas (nome)
+	VALUES ("corrida 1"),
+	 ("corrida 2"),
+	 ("corrida 3"),
+	 ("corrida 4"),
+	 ("corrida 5");
+     
+     DESC dados_corridas;
 
-SELECT * FROM participantes;
-
-SELECT * FROM equipes;
+INSERT INTO dados_corridas (id_corrida, id_equipe, tempo_volta)
+VALUES(1,1,'00:01:44');
+	SELECT * FROM participantes;
+	SELECT * FROM equipes;
+	SELECT * FROM corridas;
+	SELECT * FROM equipes_participantes;	
+	SELECT * FROM pistas;
+    SELECT * FROM dados_corridas;
